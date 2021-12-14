14787d94c4f26bda92348ca9bff8363ffe4aae1b
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-12-14 17:12:55 -0500

    adds css animation for arrow

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -21,8 +21,8 @@ function Portfolio() {
               <p>Technologies Used : Python, HTML, CSS, Django, PostgreSQL, Heroku, AWS S3</p>
               <div className='links'>
               <Link to={{ pathname: "https://github.com/platosw/chefforhire" }} target="_blank">
-               <p>See the code</p>
-              <FontAwesomeIcon icon={faGithub} className='github'/>
+               {/* <p>GitHub</p> */}
+              <FontAwesomeIcon icon={faGithub}  className='github'/>
               </Link>
               
               <Link to={{ pathname: "https://chefforhire.herokuapp.com/" }} target="_blank">
@@ -34,6 +34,7 @@ function Portfolio() {
            
               <div className="card">
               <img src={process.env.PUBLIC_URL + '/exoplanet.jpeg'} alt="Chefforhire Landing Page" />
+              <div className='card-content'> 
               <h2>ExoPlanet Logger</h2>
               <p>A Full-stack FULL-CRUD app that can create a new log for a newly discovered planet.</p>
               <p>Technologies Used : HTML, CSS, Node, Express, Mongoose, MongoDB, Heroku</p>
@@ -46,9 +47,11 @@ function Portfolio() {
               </Link>
               </div>
               </div>
+              </div>
            
               <div className="card">
               <img src={process.env.PUBLIC_URL + '/ruleofthumb.jpeg'} alt="Chefforhire Landing Page" />
+              <div className='card-content'>
               <h2>Rule of Thumb</h2>
               <p>A Full-stack decoupled message-board app that's purpose is for people to share advice of different topics.</p>
               <p>Technologies Used : React, HTML, CSS, JavaScript, Node, Express, MongoDB, Mongoose, Heroku, Netlify</p>
@@ -61,9 +64,11 @@ function Portfolio() {
               </Link>
               </div>
               </div>
+              </div>
               
               <div className="card">
               <img src={process.env.PUBLIC_URL + '/giphy.png'} alt="Chefforhire Landing Page" />
+              <div className='card-content'>
               <h2>Giphy Portal</h2>
               <p>An app that utilizes the Giphy API and displays a random Gif based on a search query. </p>
               <p>Technologies Used : HTML, CSS, JQuery, GIPHY API, JavaScript, Heroku</p>
@@ -74,6 +79,7 @@ function Portfolio() {
               <Link to={{ pathname: "https://ashcoder98.github.io/giphy-portal-app/" }} target="_blank">
               <p>Visit Wesbite</p>
               </Link>
+              </div>
               </div>
               </div>
             
