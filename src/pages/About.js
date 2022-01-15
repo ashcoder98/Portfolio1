37658212c5cf9400d4bfba2763f89a9b7c83c848
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2022-01-15 13:39:13 -0500

    adds skill badges to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,12 +9,38 @@ function About() {
              constant <span>collaboration</span> between employees of all levels. The most important fundamental skill 
              I learned there was the ability to adapt and learn something new. Apple is a powerhouse of ingenuity
              and this has forever impacted how adaptable I am to new information. I feel this has lead me to the world of
-             coding. The constant cycle of learning has kindled my passion for coding. <span>What makes coding
+             coding. The constant cycle of learning has kindled my passion for coding.
+              <br />
+             <span>What makes coding
              so amazing for me is the ability to impact others</span> and give people ways to display who they are and what they want to show the world.
              I want to be a person that builds someone up and puts a spotlight on them, but never myself. To uplift other people's vision is my dream
              in the coding world!
         </p>
-        {/* <img src="	https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="Github" /> */}
+        <h1>My Skills</h1>
+          <div className="skills">
+        <img src="	https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="Github" />
+        <img src="https://img.shields.io/badge/Slack-4A154B?style=for-the-badge&logo=slack&logoColor=white" alt="Slack" />
+        <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"  alt="HTML" />
+        <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS" />
+        <img src="	https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" alt="JavaScript" />
+        <img src="https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white" alt="Python" />
+        <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" alt="MongoDB" />
+        <img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" alt="Express.js" />
+        <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React" />
+        <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" alt="Node.js" />
+        <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap" />
+        <img src="https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white" alt="jQuery" />
+        <img src="https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white" alt="Django" />
+        <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" alt="postgreSQL" />
+        <img src="https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white" alt="Netlify" />
+        <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" alt="Heroku" />
+        
+          </div>
+
+
+
+        
+
           </div>
 
         </div>
