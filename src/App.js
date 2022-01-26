6bef4972fc4582dbed30bcefc56c2df0a2a351f2
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2022-01-25 20:12:53 -0500

    adds background for home page to make text more legible

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
     <div className="App">
       <Nav />
       <Main />
-     
-
-
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,11 +10,10 @@ function Home() {
 <div className="title" >
     <h2 className="name" >Alexander Harris</h2>
     <h4 className="job">Full-Stack Software Engineer</h4>
-</div>
-    {/* <h4 className='static-txt'>I'm a</h4>
-    <ul cla></ul> */}
+
 
     <p>Explore my projects <Link to="/portfolio"> <FontAwesomeIcon className='arrow' icon={faChevronCircleRight} size='3x'></FontAwesomeIcon> </Link> </p>
+</div>
     
         </div>
         );
